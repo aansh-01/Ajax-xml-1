0b8b42cf766dcaaa960af396d29d5466ee95f3be
annaco <conradsonanna@hotmail.com>
2018-02-11 12:13:10 +0100

    undefined gone

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function selectLandscape() {
   var landscape;
   landscape = this.value;
   requestLandscapeData(landscape);
+ 
   //this.selectedIndex = 0; //återställer menyn
 }
 
@@ -30,24 +31,25 @@ function requestLandscapeData(landscape) {
   request.open("GET", "xml/town/" + landscape + ".xml", true);
   request.send(null);
   request.onreadystatechange = function() {
-    if(request.readyState == 4)
+    if(request.readyState == 4 || request.readyState == "complete")
       if(request.status == 200) getDataLandscape(request.responseXML);
         else landscapeElem.innerHTML = "Den begärda resursen finns inte";
   };
 }
 
+
 function getDataLandscape(XMLcode) {
   var cityElems;
   var nameElem;
   var i;
-  var HTMLcode;			// Textsträng med ny HTML-kod som skapas
+  var HTMLcode = [];			// Textsträng med ny HTML-kod som skapas
 
  	cityElems = XMLcode.getElementsByTagName("city");
 
   for (i=0; i<cityElems.length; i++) {
     type = cityElems[i].getAttribute("type");
     nameElem = cityElems[i].getElementsByTagName("name")[0];
- 	  HTMLcode += nameElem.firstChild.data + "<br>";
+    HTMLcode += nameElem.firstChild.data + "<br>";
   }
   cityResultElem.innerHTML = HTMLcode;
 }
